c8982c9b8e708cc8997f07a591c4e7e70643a459
chernokoz <vthctltcw202>
2020-09-25 16:24:16 +0300

    finish solution!

--- Lectures/Lecture3/Assets/Scripts/MeshGenerator.cs
+++ Lectures/Lecture3/Assets/Scripts/MeshGenerator.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using System.Reflection;
 using Unity.Mathematics;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
@@ -44,39 +45,43 @@ public class MeshGenerator : MonoBehaviour
         private List<Vector3> vertices;
         private List<Vector3> normals;
         private List<int> indices;
+        private MetaBallField field;
+        private float size;
         
-
+    
         public Cube(float beginX, float beginY, float beginZ, float size, 
-            List<Vector3> vertices, List<Vector3> normals, List<int> indices)
+            List<Vector3> verts, List<Vector3> norms, List<int> indxs,
+            MetaBallField field)
         {
             points = new List<Vector3>
             {
-                new Vector3(beginX + 0,  beginY + 0, beginZ + 0), // 0
-                new Vector3(beginX + 0,  beginY + 1, beginZ + 0), // 1
-                new Vector3(beginX + 1,  beginY + 1, beginZ + 0), // 2
-                new Vector3(beginX + 1,  beginY + 0, beginZ + 0), // 3
-                new Vector3(beginX + 0,  beginY + 0, beginZ + 1), // 4
-                new Vector3(beginX + 0,  beginY + 1, beginZ + 1), // 5
-                new Vector3(beginX + 1,  beginY + 1, beginZ + 1), // 6
-                new Vector3(beginX + 1,  beginY + 0, beginZ + 1), // 7
+                new Vector3(beginX + 0 * size,  beginY + 0 * size, beginZ + 0 * size), // 0
+                new Vector3(beginX + 0 * size,  beginY + 1 * size, beginZ + 0 * size), // 1
+                new Vector3(beginX + 1 * size,  beginY + 1 * size, beginZ + 0 * size), // 2
+                new Vector3(beginX + 1 * size,  beginY + 0 * size, beginZ + 0 * size), // 3
+                new Vector3(beginX + 0 * size,  beginY + 0 * size, beginZ + 1 * size), // 4
+                new Vector3(beginX + 0 * size,  beginY + 1 * size, beginZ + 1 * size), // 5
+                new Vector3(beginX + 1 * size,  beginY + 1 * size, beginZ + 1 * size), // 6
+                new Vector3(beginX + 1 * size,  beginY + 0 * size, beginZ + 1 * size), // 7
             };
             
-            F_values[0] = new MetaBallField().F(points[0]);
-            F_values[1] = new MetaBallField().F(points[1]);
-            F_values[2] = new MetaBallField().F(points[2]);
-            F_values[3] = new MetaBallField().F(points[3]);
-            F_values[4] = new MetaBallField().F(points[4]);
-            F_values[5] = new MetaBallField().F(points[5]);
-            F_values[6] = new MetaBallField().F(points[6]);
-            F_values[7] = new MetaBallField().F(points[7]);
-
-            this.vertices = vertices;
-            this.normals = normals;
-            this.indices = indices;
+            F_values[0] = field.F(points[0]);
+            F_values[1] = field.F(points[1]);
+            F_values[2] = field.F(points[2]);
+            F_values[3] = field.F(points[3]);
+            F_values[4] = field.F(points[4]);
+            F_values[5] = field.F(points[5]);
+            F_values[6] = field.F(points[6]);
+            F_values[7] = field.F(points[7]);
+    
+            vertices = verts;
+            normals = norms;
+            indices = indxs;
+            this.field = field;
+            this.size = size;
         }
-        public List<Vector3> addTriangles()
+        public void addTriangles()
         {
-            var res = new List<Vector3>();
             var currentCaseMask = 0;
             for (var i = 0; i < 8; i++)
             {
@@ -85,54 +90,67 @@ public class MeshGenerator : MonoBehaviour
                     currentCaseMask |= (1 << i);
                 }
             }
-
+    
             var trianglesCount = MarchingCubes.Tables.CaseToTrianglesCount[currentCaseMask];
-            var currentCase =  MarchingCubes.Tables.CaseToVertices[currentCaseMask];
-
+            var currentCase =  MarchingCubes.Tables.CaseToVertices[currentCaseMask]; // int3 [] case
+    
             for (var i = 0; i < trianglesCount; i++)
             {
-                var currentVertices = currentCase[i];
+                var currentTriangle = currentCase[i]; // int3 with edges of current triangle
                 
-                for (var j = 0; j <= 2; j++)
+                for (var j = 0; j < 3; j++)
                 {
-                    // add first point
+                    // add triangle point
                     indices.Add(vertices.Count);
-                    vertices.Add(getEdgeZeroPoint(currentVertices[j]));
+                    vertices.Add(getEdgeZeroPoint(currentTriangle[j]));
+                    // vertices.Add(points[j]);
                     normals.Add(getNormal(vertices.Last()));
+                    
                 }
 
             }
-            
-            return res;
         }
         
         private Vector3 getEdgeZeroPoint(int edgeNumber)
         {
-            var edge = MarchingCubes.Tables._cubeEdges[edgeNumber];
-            var firstVertex = MarchingCubes.Tables._cubeVertices[edge[0]];
-            var secondVertex = MarchingCubes.Tables._cubeVertices[edge[1]];
-            var a = new MetaBallField().F(firstVertex);
-            var b = new MetaBallField().F(secondVertex);
+            var edge = MarchingCubes.Tables._cubeEdges[edgeNumber]; // current edge
+            var firstVertex = points[0] + MarchingCubes.Tables._cubeVertices[edge[0]] * size;
+            var secondVertex = points[0] + MarchingCubes.Tables._cubeVertices[edge[1]] * size;
+            var a = F_values[edge[0]];
+            var b = F_values[edge[1]];
 
-            return (firstVertex * a - secondVertex * b) / (b - a);
+            return (firstVertex * b - secondVertex * a) / (b - a);
         }
-
-        private Vector3 getNormal(Vector3 x)
+    
+        private Vector3 getNormal(Vector3 p)
         {
-            var instance = new MetaBallField();
             var deltaX = new Vector3(0.1f, 0, 0);
             var deltaY = new Vector3(0, 0.1f, 0);
             var deltaZ = new Vector3(0, 0, 0.1f);
-            var nx = instance.F(x + deltaX) - instance.F(x - deltaX);
-            var ny = instance.F(x + deltaY) - instance.F(x - deltaY);
-            var nz = instance.F(x + deltaZ) - instance.F(x - deltaZ);
-
-            return Vector3.Normalize(new Vector3(nx, ny, nz));
+            var nx = field.F(p + deltaX) - field.F(p - deltaX);
+            var ny = field.F(p + deltaY) - field.F(p - deltaY);
+            var nz = field.F(p + deltaZ) - field.F(p - deltaZ);
+    
+            return -Vector3.Normalize(new Vector3(nx, ny, nz));
         }
     }
 
     private void Update()
     {
+        vertices.Clear();
+        indices.Clear();
+        normals.Clear();
+
+        Field.Update();
+
+
+        var centerCoord = Field.getMassCenter();
+        // var meshCubes = new List<Cube>();
+        const float begin = -3f;
+        const float end = 3f;
+        const float cubeSize = 0.1f;
+        
+        
         // List<Vector3> cubeVertices = new List<Vector3>
         // {
         //     new Vector3(0, 0, 0), // 0
@@ -144,7 +162,19 @@ public class MeshGenerator : MonoBehaviour
         //     new Vector3(1, 1, 1), // 6
         //     new Vector3(1, 0, 1), // 7
         // };
-
+        //
+        // List<Vector3> cubeVertices = new List<Vector3>
+        // {
+        //     new Vector3(centerCoord[0] + begin, centerCoord[1] + begin, centerCoord[2] + begin), // 0
+        //     new Vector3(centerCoord[0] + begin, centerCoord[1] + end, centerCoord[2] + begin), // 1
+        //     new Vector3(centerCoord[0] + end, centerCoord[1] + end, centerCoord[2] + begin), // 3
+        //     new Vector3(centerCoord[0] + end, centerCoord[1] + begin, centerCoord[2] + begin), // 2
+        //     new Vector3(centerCoord[0] + begin, centerCoord[1] + begin, centerCoord[2] + end), // 4
+        //     new Vector3(centerCoord[0] + begin, centerCoord[1] + end, centerCoord[2] + end), // 5
+        //     new Vector3(centerCoord[0] + end, centerCoord[1] + end, centerCoord[2] + end), // 6
+        //     new Vector3(centerCoord[0] + end, centerCoord[1] + begin, centerCoord[2] + end), // 7
+        // };
+        //
         // int[] sourceTriangles =
         // {
         //     0, 1, 2, 2, 3, 0, // front
@@ -156,33 +186,32 @@ public class MeshGenerator : MonoBehaviour
         // };
 
 
-        vertices.Clear();
-        indices.Clear();
-        normals.Clear();
-
-        Field.Update();
 
 
-        var meshCubes = new List<Cube>();
-        const float begin = -5;
-        const float end = 5;
-        const float cube_size = 0.2f;
-        for (var i = begin; i < end; i += cube_size)
+        // var cube = new Cube(centerCoord[0], centerCoord[1], centerCoord[2], cube_size * 10,
+        //     vertices, normals, indices, Field);
+        // meshCubes.Add(cube);
+        // cube.addTriangles();
+        
+        
+        
+        for (var i = centerCoord[0] + begin; i < centerCoord[0] + end; i += cubeSize)
         {
-            for (var j = begin; i < end; i += cube_size)
+            for (var j = centerCoord[1] + begin; j < centerCoord[1] + end; j += cubeSize)
             {
-                for (var k = begin; i < end; i += cube_size)
+                for (var k = centerCoord[2] + begin; k < centerCoord[2] + end; k += cubeSize)
                 {
-                    meshCubes.Add(new Cube(i, j, k, cube_size,
-                        vertices, normals, indices));
+                    var cube = new Cube(i, j, k, cubeSize,
+                        vertices, normals, indices, Field);
+                    cube.addTriangles();
                 }
             }
         }
         
-        foreach (var meshCube in meshCubes)
-        {
-            meshCube.addTriangles();
-        }
+        // foreach (var meshCube in meshCubes)
+        // {
+        //     meshCube.addTriangles();
+        // }
 
         // ----------------------------------------------------------------
         // Generate mesh here. Below is a sample code of a cube generation.
@@ -207,8 +236,6 @@ public class MeshGenerator : MonoBehaviour
         //     vertices.Add(vertexPos);
         // }
         
-        print(vertices.Count);
-
         // Here unity automatically assumes that vertices are points and hence (x, y, z) will be represented as (x, y, z, 1) in homogenous coordinates
         _mesh.Clear();
         _mesh.SetVertices(vertices);
